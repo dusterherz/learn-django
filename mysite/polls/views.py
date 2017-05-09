285ced72d78f83398afc43be4d55901cf4464f9f
Aude Lejuez <dusterherz@gmail.com>
2017-05-09 10:50:13 +0200

    The polls view is tested

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,23 +2,29 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()
+                                       ).order_by('-pub_date')[:5]
+
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/result.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -27,7 +33,7 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/details.html', {
             'question': question,
-            'error_message':"You didn't select a choice.",
+            'error_message': "You didn't select a choice.",
         })
     else:
         selected_choice.votes += 1
